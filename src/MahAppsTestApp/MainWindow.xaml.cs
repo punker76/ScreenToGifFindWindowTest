6a6e8f8e221b0042619d21a1700db774b10b4808
punker76 <punkerat76@gmail.com>
2020-07-10 12:19:29 +0200

    Add test apps

---
+++ src/MahAppsTestApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace MahAppsTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IntPtr CriticalHandle
+        {
+            get
+            {
+                var value = typeof(Window)
+                    .GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .GetValue(this, new object[0]);
+                return (IntPtr) value;
+            }
+        }
+    }
+}
---
+++ src/WPFTestApp/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace WPFTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IntPtr CriticalHandle
+        {
+            get
+            {
+                var value = typeof(Window)
+                    .GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .GetValue(this, new object[0]);
+                return (IntPtr) value;
+            }
+        }
+    }
+}
